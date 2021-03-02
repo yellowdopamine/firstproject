5ca75cb7a423c7ee89c268f3c3cc27060ec40ace
adm1n <tulbassaeyv@gmail.com>
2021-03-03 05:06:16 +0600

    first commit

---
+++ JSFILES/js/script.js
@@ -0,0 +1,38 @@
+(function () {
+   'use strict';
+   // this function is strict...
+}());
+
+// const obj = {
+//    name: "Yeldos",
+//    age: 18,
+//    isMarried: false
+// };
+
+// console.log(obj.name);
+
+// alert('Hello');
+
+// const result = confirm("R u here?");
+// console.log(result);
+
+// const answer = prompt("Are u over 18?");
+// console.log( typeof(answer) );
+
+// const answers = [];
+
+// answers[0] = prompt('Your name: ', '');
+// answers[1] = prompt('Your last name: ', '');
+// answers[2] = prompt('Your age: ', '');
+
+// document.write(answers);
+
+// console.log(typeof (answers));
+
+const category = 'toys';
+console.log('https://someurl.com' + category);
+console.log(`https://someurl.com/${category}/5`);
+
+let incr = 10,
+    dect = 10;
+
